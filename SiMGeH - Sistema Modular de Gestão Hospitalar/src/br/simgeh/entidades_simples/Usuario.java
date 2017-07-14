58fd07ea05bb291983d330fa5b40f883ff1fbe0e
Unknown <alheirosb@gmail.com>
2017-07-14 13:41:53 -0300

    Criacao da Entidade Usuario

---
+++ SiMGeH - Sistema Modular de Gestão Hospitalar/src/br/simgeh/entidades_simples/Usuario.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.simgeh.entidades_simples;
+
+/**
+ *
+ * @author Bartô
+ */
+public class Usuario {
+    private Integer matricula;
+    private String login;
+    private String senha;
+    private Boolean admin;
+    private String   
+}
