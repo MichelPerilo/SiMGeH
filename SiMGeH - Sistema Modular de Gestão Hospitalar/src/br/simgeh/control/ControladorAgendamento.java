cc4a58cabf0861456c65127752dcd70780666b53
Michel Silva <michel.perilo@recife.pe.gov.br>
2017-08-16 12:06:05 -0300

    Tela Agendamento Consulta, Repositorio de Usuarios

--- SiMGeH - Sistema Modular de Gestão Hospitalar/src/br/simgeh/control/ControladorAgendamento.java
+++ SiMGeH - Sistema Modular de Gestão Hospitalar/src/br/simgeh/control/ControladorAgendamento.java
@@ -6,7 +6,13 @@
 package br.simgeh.control;
 
 import br.simgeh.exeception.CadastroAgendamentoExistenteException;
+import br.simgeh.exeception.CadastroServicoExistenteException;
 import br.simgeh.model.Agendamento;
+import br.simgeh.model.Consulta;
+import br.simgeh.model.Exame;
+import br.simgeh.model.ExameImagem;
+import br.simgeh.model.ExameLaboratorial;
+import br.simgeh.model.ProcCirurgico;
 import br.simgeh.model.Servico;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,6 +24,7 @@ import java.util.Date;
  */
 public class ControladorAgendamento implements IGerenciadorAgendamento {
     private  CadastroAgendamento agendamento = new CadastroAgendamento();
+    private  CadastroServico servico = new CadastroServico();
    
      
      private static IGerenciadorAgendamento instance;
@@ -30,11 +37,20 @@ public class ControladorAgendamento implements IGerenciadorAgendamento {
     }
 
    
-    public void cadatrarAgendamento(int prontuario_id, String data, String hora, Servico servico ) throws IOException, CadastroAgendamentoExistenteException{
+    public void cadatrarAgendamento(int prontuario_id, String data, String hora , int type) throws IOException, CadastroAgendamentoExistenteException, CadastroServicoExistenteException{
+        Servico s = null;
     
-    
-    Agendamento a = new Agendamento( prontuario_id,  data, hora, servico );
-    this.agendamento.cadatrar(a);     
+        switch(type){
+            case 1 : s = new Consulta();
+            case 2 : s = new ExameImagem();
+            case 3 : s = new ExameLaboratorial();
+            case 4 : s = new ProcCirurgico();
+        }
+        
+        Agendamento a = new Agendamento( prontuario_id, data, hora, s);
+        this.agendamento.cadatrar(a);     
+        this.servico.cadatrar(s);
+        
     }
 
     public ArrayList<Agendamento> agendamentosPorProntuario(int prontuario_id){
